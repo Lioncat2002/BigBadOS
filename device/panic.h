52ccbd0b9a1eec5d682b9073e8a9130ebea33996
Till Smejkal <till.smejkal@tu-dresden.de>
2026-04-27 10:39:48 +0200

    template for task 2

---
+++ device/panic.h
@@ -0,0 +1,29 @@
+/*****************************************************************************/
+/* Operating-System Construction                                             */
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                                 P A N I C                                 */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+/* Default interrupt handler.                                                */
+/*****************************************************************************/
+
+#ifndef __panic_include__
+#define __panic_include__
+
+/* INCLUDES */
+
+#include "guard/gate.h"
+
+class Panic
+/* Add your code here */ 
+{
+public:
+	Panic (const Panic &copy) = delete; // prevent copying
+	Panic& operator=(const Panic&) = delete; // prevent assignment
+	Panic () {}
+/* Add your code here */ 
+ 
+};
+
+#endif
