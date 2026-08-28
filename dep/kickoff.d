c4c85fc38527577fe087df30c23c0e881daf5319
studi <studi@BSVM>
2026-08-28 22:07:51 +0200

    trying to fix

--- dep/kickoff.d
+++ dep/kickoff.d
@@ -7,4 +7,11 @@ build/kickoff.o: thread/kickoff.cc thread/kickoff.h thread/coroutine.h \
  thread/../syscall/../thread/dispatch.h \
  thread/../syscall/../thread/entrant.h \
  thread/../syscall/../thread/../object/queue.h \
- thread/../syscall/../thread/../object/chain.h
+ thread/../syscall/../thread/../object/chain.h thread/../guard/guard.h \
+ thread/../guard/locker.h thread/../guard/../device/cgastr.h \
+ thread/../guard/../device/../object/o_stream.h \
+ thread/../guard/../device/../object/strbuf.h \
+ thread/../guard/../device/../machine/cgascr.h \
+ thread/../guard/../device/../machine/io_port.h \
+ thread/../guard/../machine/cpu.h thread/../guard/gate.h \
+ thread/../guard/../object/chain.h thread/../guard/../object/queue.h
--- thread/scheduler.cc
+++ thread/scheduler.cc
@@ -49,12 +49,9 @@ void Scheduler::kill(Entrant &that){
 }
 
 void Scheduler::schedule() {
-	Entrant *next = (Entrant*)ready_queue.dequeue();
-    if(next){
-        Entrant *current = (Entrant*)active();
-        if(current){
-            ready_queue.enqueue(current);
-        }
-        dispatch(*next);
-    }
+	Entrant* next = (Entrant*)ready_queue.dequeue();
+	if (next) {
+        
+		go(*next);
+	}
 }
